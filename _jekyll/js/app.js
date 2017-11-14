0bd380453467c162706462015bda3abea85b3e68
Jared White <jared@jaredwhite.com>
2017-11-14 11:33:22 -0800

    Bunch o' shopping cart stuff

--- _jekyll/js/app.js
+++ _jekyll/js/app.js
@@ -1,1 +1,21 @@
 $(document).foundation();
+
+Snipcart.subscribe('cart.ready', function (data) {
+  updateCartCount();
+});
+Snipcart.subscribe('item.added', function (item) {
+  updateCartCount();
+});
+Snipcart.subscribe('item.removed', function (item) {
+  updateCartCount();
+});
+
+function updateCartCount() {
+  var count = Snipcart.api.items.count();
+  if (count > 0) {
+    count = "(" + count + ")"
+  } else {
+    count = "";
+  }
+  $('#cart-icon .item-count').text(count);
+}
