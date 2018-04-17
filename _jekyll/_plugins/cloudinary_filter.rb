4fbf3e366d14971cb6105b8eb99be3c4ffb063b7
Jared White <jared@jaredwhite.com>
2018-04-16 19:44:30 -0700

    Lots more homepage awesomeness

--- _jekyll/_plugins/cloudinary_filter.rb
+++ _jekyll/_plugins/cloudinary_filter.rb
@@ -1,8 +1,6 @@
 module Jekyll
   module CloudinaryFilter
     def cloudinary_url(input, attr_string="", image_format='jpg')
-      puts ENV.inspect
-
       cloudinary_url = Cloudinary::Utils.cloudinary_url(input, {format: image_format})
       if attr_string.present?
         cloudinary_url.sub!("/image/upload", "/image/upload/#{attr_string}")
@@ -12,13 +10,6 @@ module Jekyll
   end
 end
 
-###############################
-# THIS ISN'T ACTUALLY TRUE!!!!!
-# ########################################
-# API Key and Secret are set via ENV vars:
-# CLOUDINARY_API_KEY
-# CLOUDINARY_API_SECRET
-########################
 Cloudinary.config({
   "cloud_name" => 'mariposta',
   "secure" => true
